a9adc30dfd6d7207358f24810af16488044d2349
Anselmo <anselmohenrique191@gmail.com>
2023-05-17 14:57:19 -0300

    alteração

--- TODO/components/Assemble.js
+++ TODO/components/Assemble.js
@@ -1,15 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Pressable,Image} from 'react-native';
+import { StyleSheet, Text, View,Pressable,Image, TextInput} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 const Assemble =() =>{
     return(
         <View>
-                  <Image style={styles.logo} source={require('../assets/Logo.png')} />
+            <Image style={styles.logo} source={require('../assets/Logo.png')} />
+             <TextInput style={styles.inp}> </TextInput>
+             <Pressable style={styles.btn}><Text>+</Text></Pressable>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    logo:{
+        bottom:300,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        marginLeft:70,
+    },
+    inp:{
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        flexDirection:'row',
+        backgroundColor:'#2F2B2B',
+        bottom:250,
+        width:260,
+        padding:10,
+    
+    },
+    btn:{
+        width:57,
+        height:58,
+        backgroundColor:'#3abea6',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+    }
+  
 })
 
 export default Assemble
